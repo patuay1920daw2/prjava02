ab47c26f183b79d576077616c16e19849e678a7e
patuay1920daw2 <patuay1920daw2@gmail.com>
2019-11-21 19:07:51 +0100

    Treballant amb ordres Git i branques: Versió 1.2 del projecte prjava02

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -9,7 +9,7 @@ import java.net.*;
 
 /**
  *
- * @author --> Escriu el teu correu electrònic
+ * @author --> Escriu el teu correu electrÃ²nic
  */
 public class Prjava02 {
 
@@ -17,16 +17,17 @@ public class Prjava02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("versió 0.1 del projecte prjava02"); 	
+        System.out.println("versiÃ³ 0.1 del projecte prjava02"); 	
         try {
-            InetAddress adreça = InetAddress.getLocalHost();
+            InetAddress adreça= InetAddress.getLocalHost();
             String hostname = adreça.getHostName();
             System.out.println("hostname="+hostname);
             System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
             System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
             System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
-            System.out.println("Versió OS: " + System.getProperty("os.version"));
-            System.out.println("Creació d'una branca del projecte prjava02");
+            System.out.println("VersiÃ³ OS: " + System.getProperty("os.version"));
+            System.out.println("CreaciÃ³ d'una branca del projecte prjava02");
+            System.out.println("Afegint més codi a la branca00 del projecte prjava02");
         }
         catch (IOException e) {
 		System.out.println("Exception occurred");
